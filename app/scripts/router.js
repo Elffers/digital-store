c7e80596aa89f4c225d6279fb78d8c4c3649044b
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-19 16:04:39 -0700

    something

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -5,12 +5,12 @@ App.Router.map(function(){
     this.resource('product', { path: ':product_id' })
   });
 
-  this.resource('cart', { path: 'cart/:cart_id' }, function(){
-    this.route('checkout')
-  });
-
   this.resource('orders', function(){
     this.resource('order', { path: ':order_id' })
+  });
+
+  this.resource('cart', { path: 'cart/:cart_id' }, function(){
+    this.route('checkout')
   });
 
   this.route('admin');
