66b97ee439d07c0579cf8e96f7dbcbb4ba2881c2
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-21 10:50:43 -0700

    checkout is broken

--- app/scripts/routes/cart/checkout.js
+++ app/scripts/routes/cart/checkout.js
@@ -8,26 +8,33 @@ App.CartCheckoutRoute = Ember.Route.extend({
       var self = this;
       this.controllerFor('application').get('cart').then(function(cart){
         var order = this.store.createRecord("order", proxy);
-        self.store.find("cart", localStorage.cart_id).then(function(cart){
-          order.set('status', 'pending');
-          order.set('cart', cart);
-          order.set('total', cart.get('total'));
-          order.save().then(function(order){
-            var cart = self.store.createRecord('cart')
-            self.controllerFor('application').set('cart')
+        // self.store.find("cart", localStorage.cart_id).then(function(cart){
+        order.set('status', 'pending');
+        order.set('cart', cart);
+        order.set('total', cart.get('total')
+        // })
+        order.save().then(function(order){
+        // if saves, success, and then
+          // creates new cart
+          var cart = self.store.createRecord('cart');
 
-              // (reset cart)
-              self.transitionTo("order", order)
-            },
-            function(error){
-              console.log('OOPS');
-              order.deleteRecord();
-              alert(error.responseText)
-            }
-          );
+          // saves cart
+          // resets localstorage
+          cart.save().then(function(cart){
+          localStorage.cart_id = cart.cart_id
+          self.controllerFor('application').set('cart', self.store.find("cart", localStorage.cart_id))
+        })
+          // resets app controller cart attribute
+          // transition to order
+
+        function(error){
+          console.log('OOPS');
+          order.deleteRecord();
+          alert(error.responseText)
         });
-      })
-    }
+        self.transitionTo("order", order)
+      });
+    })
   },
 
   renderTemplate: function() {
